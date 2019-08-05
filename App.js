326fa06b2f1ac33212d3e2e0343b5ebc62f2f7ca
wiliamtaesunglee <wiliam.tae@gmail.com>
2019-08-05 10:19:26 -0300

    work project git upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { YellowBox } from 'react-native'
+import EventList from './EventList'
+
+YellowBox.ignoreWarnings([
+  'Warning: componentWillMount is deprecated',
+  'Warning: componentWillReceiveProps is deprecated'
+])
+
+export default function App() {
+  return (
+    <EventList/>
+  );
+}
+
+
---
+++ EventList.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { FlatList, Text, StyleSheet } from 'react-native'
+
+import EventCard from './EventCard'
+
+const styles = StyleSheet.create({
+    lsit: {
+        flex: 1,
+        paddingTop: 20,
+        backgroundColor: '#f3f3f3' 
+    }
+})
+
+class EventList extends Component {
+    state = {
+        events: []
+    }
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                events: this.state.events.map(event => ({
+                    ...event,
+                    timer: Date.now(),
+                })),
+            })    
+        }, 1000);
+
+        const events = require('./db.json').events.map(e => ({
+            ...e,
+            date: new Date(e.date)
+        }))
+        this.setState({events})
+    }
+
+    render() {
+        return(
+            <FlatList style={styles.lsit}
+                data={this.state.events}
+                renderItem={({item}) => <EventCard event={item}/>}
+                keyExtractor={item => item.id}
+            />
+        )
+    }
+}
+
+export default EventList
